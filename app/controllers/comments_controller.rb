b12e1c0f3cf9d9810141427a5eef95feabf1e429
Sebastian Wilgosz <sebastian.wilgosz@gmail.com>
2015-10-30 22:59:43 +0100

    added option to fix or not n+1 query for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,9 @@ class CommentsController < ApplicationController
   # GET /comments
   # GET /comments.json
   def index
-    @comments = Comment.all.includes(:article) #.limit(5)
+    scope = Comment.all
+    @comments = params[:fix].present? ? scope.includes(:article) : scope
+
   end
 
   # GET /comments/1
